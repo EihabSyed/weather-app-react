bdd587bd00e23843f74981994704dfdb8c0f9c10
Eihab Syed <eihabsyed@gmail.com>
2021-05-05 19:47:58 -0400

    Styled ToolBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from './images/logo.svg';
 import './App.css';
+import WeatherPage from './components/weather-data.js';
+import ToolBar from './components/toolbar.js';
 
 function App() {
   return (
     <div className="App">
+      <ToolBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,6 +21,7 @@ function App() {
         >
           Learn React
         </a>
+        <WeatherPage />
       </header>
     </div>
   );
